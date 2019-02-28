07d97a466a879200b2f4c1f3555d74941d25e026
Amir Mofasser <amir.mofasser@gmail.com>
2019-02-28 15:53:37 +0100

    Remove dark mode
    Dark mode adds too much complexity and is to be removed. Maybe add it the future.

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <div id="app" :style="{'background-color': variant}">
+  <div id="app">
     <Nav/>
     <router-view/>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
 import Nav from './components/Nav.vue'
 export default {
   name: 'App',
   components: {
     Nav,
-  },
-  computed: {
-    ...mapState([
-      'theme'
-    ]),
-    variant() {
-      if(this.theme == 'dark') {
-        return '#212429'
-      } else {
-        return '#fff'
-      }
-    }  
-  },
+  }
 }
 </script>
 
--- web/src/components/Nav.vue
+++ web/src/components/Nav.vue
@@ -5,10 +5,9 @@
       
       <Breadcrumb class="col" />
       <div class="row text-right align-items-center">
-        <switches class="col" v-model="isDark" color="default"></switches>
         <span class="app-ver col">v1.0.0</span>
         <span class="github-link col">
-          <a href="https://github.com/amimof/logga"><i class="fab fa-github text-dark"></i></a>
+          <a href="https://github.com/amimof/logga" target="_blank"><i class="fab fa-github text-dark"></i></a>
         </span>
       </div>
     </div>
@@ -16,30 +15,15 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
 import Breadcrumb from './Breadcrumb.vue'
-import Switches from 'vue-switches';
+
+let body;
 
 export default {
   name: 'Nav',
   components: {
-    Breadcrumb,
-    Switches
-  },
-  computed: {
-    ...mapState([
-      'theme'
-    ]),
-    isDark: {
-      get() {
-        return this.theme == 'dark' ? true : false;
-      },
-      set(val) {
-        this.$store.dispatch('setTheme', val ? 'dark' : 'light')
-      }
-    }
+    Breadcrumb
   }
-
 }
 </script>
 
@@ -52,9 +36,6 @@ export default {
   color: #1DE9B6;
   -ms-flex: 0 0 80px;
   flex: 0 0 80px;
-}
-.vue-switcher {
-  margin: 0;
 }
 .app-ver {
   font-size: 12px;
